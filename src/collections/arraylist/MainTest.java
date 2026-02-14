0033b4f39f0f7aa20f1c4c9aa6f52f85734a8efc
prasanrajj <prasanrajj@gmail.com>
2026-02-14 15:24:42 +0530

    MainTest

---
+++ src/collections/arraylist/MainTest.java
@@ -0,0 +1,13 @@
+package collections.arraylist;
+
+public class MainTest {
+    public static void main(String[] args) {
+
+        System.out.println("Total args: " + args.length);
+
+        for(String s : args){
+            System.out.println(s);
+        }
+    }
+}
+
