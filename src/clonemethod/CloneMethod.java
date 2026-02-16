ba7f4cd21f5c962818f29966b009dafd7406a9fe
prasanrajj <prasanrajj@gmail.com>
2026-02-16 12:48:02 +0530

    clone

---
+++ src/clonemethod/CloneMethod.java
@@ -0,0 +1,63 @@
+package clonemethod;
+
+class Location {
+	String city;
+	int pin;
+
+	Location(String city, int pin) {
+		this.city = city;
+		this.pin = pin;
+	}
+
+	// copy constructor (deep copy helper)
+	Location(Location other) {
+		this.city = other.city;
+		this.pin = other.pin;
+	}
+
+	public String toString() {
+		return city + " - " + pin;
+	}
+}
+
+class User implements Cloneable {
+	String name;
+	Location location;
+
+	User(String name, Location location) {
+		this.name = name;
+		this.location = location;
+	}
+
+//    @Override
+//    public User clone() throws CloneNotSupportedException {
+//        return (User) super.clone(); // shallow copy
+//    }
+
+	@Override
+	public User clone() throws CloneNotSupportedException {
+		// original.location == copy.location (TRUE)
+		// ⚠️ Still shallow copy.
+		User copy = (User) super.clone(); 
+		
+		// deep copy, original.location == copy.location (FALSE)
+		// Now changing clone won’t affect original.
+		copy.location = new Location(this.location); 
+		return copy;
+	}
+
+	public String toString() {
+		return name + " lives at " + location;
+	}
+}
+
+public class CloneMethod {
+	public static void main(String[] args) throws Exception {
+		Location addr = new Location("Hyderabad", 500001);
+		User u1 = new User("Prasanna", addr);
+		User u2 = u1.clone(); // shallow copy
+		u2.location.city = "Delhi";
+		System.out.println("Original: " + u1);
+		System.out.println("Cloned  : " + u2);
+	}
+}
